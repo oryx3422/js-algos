c3e759f3ef1678918b9f430e161a8e002ebdc7c7
nikblago <159848859+oryx3422@users.noreply.github.com>
2025-12-02 13:19:33 +0300

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,3 +1,8 @@
+//  arr.filter((_, i) => i % 2 === 0)
+//  arr.filter((_, i) => i !== removeIndex)
+
+//  js
+
 function myFilter(arr, callback) {
   const result = []
   for(let i = 0; i < arr.length; i++) {
@@ -6,4 +11,17 @@ function myFilter(arr, callback) {
   return result
 }
 
+
 console.log(myFilter([1, 2, 3, 4, 5], n => n % 2 === 0))
+
+//  ts
+
+function filter<T>(
+  numbers: Array<T>, 
+  callback: (item: T) => boolean) {
+  const res: Array<T> = []
+  numbers.forEach((n) => {
+    if(callback(n)) res.push(n)
+  })
+  return res
+}
